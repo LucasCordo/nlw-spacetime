6af3c68321a2267c5e7cc3cdef2051de74537936
Lucas Cordó <cordolucas59@gmail.com>
2023-06-23 00:32:17 -0300

    aula 3

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar'
 import { useEffect } from 'react'
+import { useRouter } from 'expo-router'
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 import { styled } from 'nativewind'
 import * as SecureStore from 'expo-secure-store'
@@ -20,14 +21,16 @@ import { api } from '../src/lib/api'
 
 const StyledStripes = styled(Stripes)
 
-const discovery = {
-  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
-  tokenEndpoint: 'https://github.com/login/oauth/access_token',
-  revocationEndpoint:
-    'https://github.com/settings/connections/applications/9957ece8533629959d19',
-}
-
 export default function App() {
+  const router = useRouter()
+
+  const discovery = {
+    authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+    tokenEndpoint: 'https://github.com/login/oauth/access_token',
+    revocationEndpoint:
+      'https://github.com/settings/connections/applications/9957ece8533629959d19',
+  }
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
@@ -45,22 +48,23 @@ export default function App() {
     discovery,
   )
 
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+
+    const { token } = response.data
+
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
+
   useEffect(() => {
     if (response?.type === 'success') {
       const { code } = response.params
 
-      api
-        .post('/register', {
-          code,
-        })
-        .then((response) => {
-          const { token } = response.data
-
-          SecureStore.setItemAsync('token', token)
-        })
-        .catch((err) => {
-          console.error(err)
-        })
+      handleGithubOAuthCode(code)
     }
   }, [response])
 
@@ -71,25 +75,27 @@ export default function App() {
   return (
     <ImageBackground
       source={blurBg}
-      className="relative flex-1 items-center bg-gray-900  px-8 py-10"
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
       imageStyle={{ position: 'absolute', left: '-100%' }}
     >
       <StyledStripes className="absolute left-2" />
 
       <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
+
         <View className="space-y-2">
           <Text className="text-center font-title text-2xl leading-tight text-gray-50">
             Sua cápsula do tempo
           </Text>
-          <Text className="text-center font-body text-base leading-relaxed text-gray-200">
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
             Colecione momentos marcantes da sua jornada e compartilhe (se
             quiser) com o mundo!
           </Text>
         </View>
+
         <TouchableOpacity
-          activeOpacity={0.8}
-          className=" rounded-full bg-green-500 px-5 py-3"
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
           onPress={() => SignInWithGitHub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
@@ -101,6 +107,7 @@ export default function App() {
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
         Feito com 💜 no NLW da Rocketseat
       </Text>
+
       <StatusBar style="light" translucent />
     </ImageBackground>
   )
